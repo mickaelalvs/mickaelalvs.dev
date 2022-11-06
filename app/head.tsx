d54d677ce5a0a3a1c096aebae124a557ef2608da
CruuzAzul <alves.mckl@gmail.com>
2022-11-06 15:49:53 +0100

    🤯 Update head meta

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Head() {
   return (
     <head>
-      <title>Next.js</title>
+      <title>Mickaël Alves</title>
+      <link rel="icon" href="images/logo.png" />
+      <link rel="apple-touch-icon" href="images/logo.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
   )
