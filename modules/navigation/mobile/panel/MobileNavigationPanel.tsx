da5599f7fa610414f9faf94ee9a24e4a23f95fca
CruuzAzul <alves.mckl@gmail.com>
2024-11-16 22:38:34 +0100

    📲 Remove theme switcher mobile view

--- modules/navigation/mobile/panel/MobileNavigationPanel.tsx
+++ modules/navigation/mobile/panel/MobileNavigationPanel.tsx
@@ -3,7 +3,6 @@ import {FC, HTMLAttributes} from 'react';
 import classNames from 'classnames';
 import {IconLink} from '../../links/IconLink';
 import {Navbar} from '../../Navbar';
-import ThemeSwitcher from '../../../header/ThemeSwitcher';
 
 type Props = HTMLAttributes<HTMLElement> & {expanded: boolean};
 
@@ -12,7 +11,6 @@ export const MobileNavigationPanel: FC<Props> = ({expanded, className, ...props}
     <Navbar />
     <div className={styles.footerPanel}>
       <IconLink />
-      <ThemeSwitcher />
     </div>
   </div>
 );
