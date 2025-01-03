c7e297d530a76a314de1fa937a6306e43689ce9d
CruuzAzul <alves.mckl@gmail.com>
2025-01-03 23:31:20 +0100

    🔙 Adding go back button

--- modules/speaking/talk/Talk.tsx
+++ modules/speaking/talk/Talk.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import {TalkCard} from './TalkCard';
 import React from 'react';
 import {Speaking} from '../../../config/speaking/speaking';
+import Link from 'next/link';
+import {FaAnglesLeft} from 'react-icons/fa6';
 
 interface TalkProps {
   talkData: Speaking;
@@ -12,11 +14,13 @@ export const Talk = ({talkData}: TalkProps) => {
   return (
     <section className={styles.talkRoot}>
       <div className={styles.talk}>
+        <Link href="/speaking" className={styles.goBack}>
+          <FaAnglesLeft />
+          Revenir à la liste
+        </Link>
         <h2>{talkData.title}</h2>
         <div className={styles.details}>
-          <div className={styles.infos}>
-            <p className={styles.description}>{talkData.description}</p>
-          </div>
+          <p className={styles.description}>{talkData.description}</p>
           <Image src={talkData.image} alt={talkData.title} width={320} height={180} />
         </div>
         <div className={styles.conferences}>
@@ -27,22 +31,24 @@ export const Talk = ({talkData}: TalkProps) => {
             ))}
           </div>
         </div>
-        {
-          talkData.videoId ? (
-            <div className={styles.video}>
-              <h3>Rediffusion</h3>
-              <iframe
-                className={styles.videoIframe}
-                src={`https://www.youtube.com/embed/${talkData.videoId}`}
-                title={talkData.title}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                loading='lazy'
-              />
-            </div>
-          ) : null
-        }
+        {talkData.videoId ? (
+          <div className={styles.video}>
+            <h3>Rediffusion</h3>
+            <iframe
+              className={styles.videoIframe}
+              src={`https://www.youtube.com/embed/${talkData.videoId}`}
+              title={talkData.title}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              loading="lazy"
+            />
+          </div>
+        ) : null}
+        <Link href="/speaking" className={styles.goBack}>
+          <FaAnglesLeft />
+          Revenir à la liste
+        </Link>
       </div>
     </section>
   );
-}
+};
