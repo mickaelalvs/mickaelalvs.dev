a53d6a94f34839c40d852b4eb4570a936d95f6ea
malves.externe <malves.externe@bedrockstreaming.com>
2025-11-24 09:29:42 +0100

    🎧 update podcasts links

--- data/podcasts.js
+++ data/podcasts.js
@@ -3,12 +3,11 @@ const zenikast = [
     date: '2024',
     title: 'Le Hype Driven Development',
     description: 'Dans un monde où les langages, les outils et les méthodes évoluent à un rythme effréné, la veille technologique est devenue un pilier essentiel pour les équipes de développement. Mais face à cette avalanche de nouveautés, une question cruciale se pose : les équipes doivent-elles céder à chaque tendance ?',
-    url: 'https://www.youtube.com/watch?v=3tBFOo8Yjoo',
     platforms: [
-      { name: 'Spotify', url: 'https://open.spotify.com/show/5zKv7W4Zm5B6t2Q8mGQZ2Q' },
-      { name: 'Apple Podcast', url: 'https://podcasts.apple.com/fr/podcast/les-communaut%C3%A9s-it/id1569031417' },
-      { name: 'Deezer', url: 'https://www.deezer.com/fr/show/2570912' },
-      { name: 'YouTube', url: 'https://www.youtube.com/channel/UC2V4Zo3K4Zm5B6t2Q8mGQZ2Q' }
+      { name: 'Spotify', url: 'https://open.spotify.com/episode/39LX0b34plhcYzUkMIUEaf?si=ea25ca14549e4dac' },
+      { name: 'Apple Podcast', url: 'https://podcasts.apple.com/fr/podcast/zenikast-s01e03-la-hype-driven-development/id1750454113?i=1000666221881' },
+      { name: 'Deezer', url: 'https://link.deezer.com/s/31H086hBhsbvoRQZLQcqv' },
+      { name: 'YouTube', url: 'https://www.youtube.com/watch?v=3tBFOo8Yjoo' }
     ],
     lang: '🇫🇷'
   },
@@ -16,12 +15,11 @@ const zenikast = [
     date: '2024',
     title: 'Les communautés IT',
     description: 'Dans le monde de la tech, les communautés jouent un rôle central en permettant de faire vivre des projets, de fédérer des talents et de créer de l\'émulation autour d\'initiatives inspirantes. Que ce soit en entreprise, dans l\'open source ou à travers des programmes comme GDG, Devfest, Women Techmakers, ou encore le programme Heroes, ces communautés sont un moteur d\'innovation et de collaboration.',
-    url: 'https://www.youtube.com/watch?v=wtLd2ABm0Xs',
     platforms: [
-      { name: 'Spotify', url: 'https://open.spotify.com/show/5zKv7W4Zm5B6t2Q8mGQZ2Q' },
-      { name: 'Apple Podcast', url: 'https://podcasts.apple.com/fr/podcast/les-communaut%C3%A9s-it/id1569031417' },
-      { name: 'Deezer', url: 'https://www.deezer.com/fr/show/2570912' },
-      { name: 'YouTube', url: 'https://www.youtube.com/channel/UC2V4Zo3K4Zm5B6t2Q8mGQZ2Q' }
+      { name: 'Spotify', url: 'https://open.spotify.com/episode/3qXrrV4wCitoAUhmXBcM88?si=776ba0bf0b3c4144' },
+      { name: 'Apple Podcast', url: 'https://podcasts.apple.com/fr/podcast/zenikast-s01e04-les-communaut%C3%A9s-it/id1750454113?i=1000669791692' },
+      { name: 'Deezer', url: 'https://link.deezer.com/s/31H0aROYjAhmDJMQuOMm5' },
+      { name: 'YouTube', url: 'https://youtu.be/wtLd2ABm0Xs?si=Gu7xTS-46NqKgd0Y' }
     ],
     lang: '🇫🇷'
   },
@@ -29,11 +27,11 @@ const zenikast = [
     date: '2024',
     title: 'Être junior dans la tech',
     description: 'Débuter dans le monde de la tech en tant que junior peut être à la fois excitant et intimidant. Entre les attentes, les défis techniques, et la nécessité de trouver sa place dans une équipe, le parcours d\'un développeur junior est semé d\'embûches mais aussi de belles opportunités.',
-    url: 'https://www.youtube.com/watch?v=4yBqGEsjy2E',
     platforms: [
-      { name: 'Spotify', url: 'https://open.spotify.com/episode/41mJ25fVQNBY9EQbCV6a1P?si=HR5YG35dRu2mHjICxRAcYA' },
-      { name: 'Deezer', url: 'https://dzr.page.link/1mbz8nq7B63Sjcaw5' },
-      { name: 'YouTube', url: 'https://www.youtube.com/watch?v=4yBqGEsjy2E' }
+      { name: 'Spotify', url: 'https://open.spotify.com/episode/41mJ25fVQNBY9EQbCV6a1P?si=b27f08ea65b24914' },
+      { name: 'Apple Podcast', url: 'https://podcasts.apple.com/fr/podcast/zenikast-s02e03-etre-junior-dans-la-tech/id1750454113?i=1000710108146' },
+      { name: 'Deezer', url: 'https://link.deezer.com/s/31H0b8L7Zjv1HnFCg0loa' },
+      { name: 'YouTube', url: 'https://youtu.be/4yBqGEsjy2E?si=QRD8x7jLhLEfEq7K' }
     ],
     lang: '🇫🇷'
   }
